c626c203ee8a0d082f5268d2c6fd43f38ef65805
zekebaldwin <96504519+zekebaldwin@users.noreply.github.com>
2022-02-15 11:01:02 -0500

    Add files via upload
    updated files

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const form = document.querySelector('#formid')
 const btn = document.querySelector('#btn')
-const text = document.querySelector('#text')
+const removeButton = document.querySelector('#remove')
 const div = document.querySelector('#imageContainer')
 
 const gifSearch = async function(search){
     const results = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=BTGNnq4FQSEXmbQC4mFM8e2ACFo7r9tC`)
     const numResults = Math.floor(Math.random() * 33)
-    // console.log(results.data.data[0].images.original.url)
     const image = document.createElement('img')
     image.src = results.data.data[numResults].images.original.url
     div.appendChild(image) 
@@ -14,13 +13,14 @@ const gifSearch = async function(search){
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
-    let searchValue = e.target[1].value
+    const input = document.querySelector('#text')
+    let searchValue = input.value
     gifSearch(searchValue)
     form.reset()
 })
 
-div.addEventListener('click', function(){
-    div.remove()
+removeButton.addEventListener('click', function(){
+    div.innerHTML = ''
 })
 
 
