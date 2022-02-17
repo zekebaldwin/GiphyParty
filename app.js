136b81f586baa387d8ec85292a06707e2c5bbb7d
halim.tannous <halim.tannous@gmail.com>
2022-02-17 21:38:23 +0100

    changes post mentor session

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
-const form = document.querySelector('#formid')
-const btn = document.querySelector('#btn')
-const removeButton = document.querySelector('#remove')
-const div = document.querySelector('#imageContainer')
+const form = document.querySelector("#formid");
+const btn = document.querySelector("#btn");
+const removeButton = document.querySelector("#remove");
+const div = document.querySelector("#imageContainer");
 
-const gifSearch = async function(search){
-    const results = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=BTGNnq4FQSEXmbQC4mFM8e2ACFo7r9tC`)
-    const numResults = Math.floor(Math.random() * 33)
-    const image = document.createElement('img')
-    image.src = results.data.data[numResults].images.original.url
-    div.appendChild(image) 
+const apiKey = "BTGNnq4FQSEXmbQC4mFM8e2ACFo7r9tC";
+
+const gifSearch = async function (search) {
+  const results = await axios.get(
+    `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}`
+  );
+  const numResults = Math.floor(Math.random() * 33);
+  return results.data.data[numResults].images.original.url;
+};
+
+function createImage(src) {
+  const image = document.createElement("img");
+  image.src = src;
+  return image;
 }
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const input = document.querySelector("#text");
+  let searchValue = input.value;
+  const imageSrc = gifSearch(searchValue);
+  const img = createImage(imageSrc);
+  div.appendChild(img);
+  form.reset();
+});
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    const input = document.querySelector('#text')
-    let searchValue = input.value
-    gifSearch(searchValue)
-    form.reset()
-})
-
-removeButton.addEventListener('click', function(){
-    div.innerHTML = ''
-})
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
+removeButton.addEventListener("click", function () {
+  div.innerHTML = "";
+});
